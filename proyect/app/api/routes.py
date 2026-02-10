598d9730edbf94088ac7efff3728ff5528189361
Miguel Quetglas <mquetglas04@gmail.com>
2026-02-10 22:20:16 +0100

    Update routes.py

--- proyect/app/api/routes.py
+++ proyect/app/api/routes.py
@@ -1,13 +1,8 @@
-diff --git a/proyect/app/api/routes.py b/proyect/app/api/routes.py
-index f397d90eddb4169bf80b2611cc6d4e5ffe814486..75ab15ba3dc85f2c94c3410da0e7da10b22de651 100644
---- a/proyect/app/api/routes.py
-+++ b/proyect/app/api/routes.py
-@@ -1,30 +1,28 @@
+
  from __future__ import annotations
  
  from datetime import datetime, timezone
--from typing import Annotated
--
+
  from fastapi import APIRouter, Depends, HTTPException, Query, status
  from sqlalchemy import and_, func, select, text
  from sqlalchemy.orm import Session
